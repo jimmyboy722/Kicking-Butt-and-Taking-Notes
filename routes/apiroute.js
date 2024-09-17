bcfd14e48886bf6907dec01ac2c566ccbb4e7a1f
jimmyboy722 <jeckman722@gmail.com>
2024-09-17 13:56:48 -0400

    changed html route path and fixed uuid error

--- routes/apiroute.js
+++ routes/apiroute.js
@@ -5,7 +5,7 @@ const noteHandler = require("../db/Notehandler");
 // IMPORTING THE FS MODULE TO READ CONTENTS OF DB.JSON FILE
 const fs = require("fs");
 
-router.get("/api/notes", (req, res) => {
+router.get("/notes", (req, res) => {
   fs.readFile("db.json", "utf-8", (err, data) => {
     if (err) {
       console.error(err);
@@ -18,7 +18,7 @@ router.get("/api/notes", (req, res) => {
   });
 });
 
-router.post("/api/notes", (req, res) => {
+router.post("/notes", (req, res) => {
   noteHandler.removeNote(req.params.id);
   if (err) console.error(err);
   res.status(500).json({ Error: "Note Failed!" });
